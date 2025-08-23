759a10a2673420aeaba9767fe0c7fcd7a1edfd04
Marc <marc@bouts.be>
2025-08-23 13:52:55 +0200

    admin(Member): toon Contact & adres + gezinsleden inline via patch; branding NL

--- core/apps.py
+++ core/apps.py
@@ -1,32 +1,10 @@
-from django.apps import AppConfig, apps
+from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core"
-    verbose_name = "Fakturatie"
+    verbose_name = "Spiegelven"
 
     def ready(self):
-        from . import numbering  # activeer nummering
-
-        # Zet NL modelnamen in de admin
-        def set_names(model_label, enkelvoud, meervoud):
-            try:
-                m = apps.get_model("core", model_label)
-                m._meta.verbose_name = enkelvoud
-                m._meta.verbose_name_plural = meervoud
-            except LookupError:
-                pass
-
-        set_names("Member", "Lid", "Leden")
-        set_names("Household", "Gezin", "Gezinnen")
-        set_names("InvoiceAccount", "Facturatieaccount", "Facturatieaccounts")
-        set_names("Invoice", "Factuur", "Facturen")
-        set_names("InvoiceLine", "Factuurregel", "Factuurregels")
-        set_names("Product", "Product", "Producten")
-        set_names("YearPlan", "Jaarplan", "Jaarplannen")
-        set_names("YearPlanItem", "Jaarplan-onderdeel", "Jaarplan-onderdelen")
-        set_names("YearSequence", "Nummerreeks", "Nummerreeksen")
-        set_names("ImportMapping", "Importkoppeling", "Importkoppelingen")
-        set_names("PricingRule", "Prijsregel", "Prijsregels")
-        set_names("MemberAsset", "Ledenvoorziening", "Ledenvoorzieningen")
-        set_names("OrganizationProfile", "Organisatieprofiel", "Organisatieprofielen")
+        from . import admin_member_patch
+        from . import nl_labels  # noqa
