d7b968eb485c989c5674bf4f059274a4ad33c299
Boutsy <marc@bouts.be>
2025-09-15 19:11:23 +0200

    admin: temporarily auto-register all core models

--- core/admin.py
+++ core/admin.py
@@ -1,1 +1,10 @@
 from django.contrib import admin
+from django.apps import apps
+
+# Registreer alle modellen van de app "core" met de default ModelAdmin
+core_app = apps.get_app_config("core")
+for model in core_app.get_models():
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
